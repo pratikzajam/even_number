d3f94116326787ae005e3eb06a1e60e0a47d840c
PratikZajam <zajampratik@gmail.com>
2024-10-05 00:37:16 +0530

    added odd number code as well

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = numbers.filter(num => num % 2 === 0);
 
 console.log(evenNumbers);  
+
+
+
+const oddNumbers = numbers.filter(num => num % 2 !== 0);
+
+console.log(oddNumbers);  // Output: [1, 3, 5, 7, 9]
+
